4ebdf49638bce3eca60af3089695ef8008d1e917
chanux <chanuka@thinkcube.com>
2017-09-23 22:21:52 +0800

    Fixes formatting Improves README

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Trying to implement [python adventure](https://github.com/thinkcube/python-adven
 
 Build as follows
 
-    go build -o advgo adv.go
+    go build -o goadv adv.go
 
 Run as follows
 
-    ./advgo
+    ./goadv
 
 Check it as follows
 
@@ -21,7 +21,7 @@ Check it as follows
 
 Static build
 
-    CGO_ENABLED=0 go build -o advent -a -ldflags '-s' adv.go
+    CGO_ENABLED=0 go build -o goadv -a -ldflags '-s' adv.go
 
 I build it with static linking to put it in a docker image.
 
@@ -29,8 +29,9 @@ Build docker image as follows
 
     docker build -t chanux/advgo
 
-Run docker container as follows (This works even if you din't build. I pushed 
-[my image](https://registry.hub.docker.com/u/chanux/advgo/) to docker hub)
+Run docker container as follows (This works even if you didn't build since I've
+pushed [my image](https://registry.hub.docker.com/u/chanux/advgo/) to docker
+hub)
 
     docker run -it --rm -p 9000:9000 chanux/advgo
 
--- adv.go
+++ adv.go
@@ -1,13 +1,13 @@
 package main
 
-import(
-    "bufio"
-    "fmt"
-    "net/http"
-    "os"
-    "strings"
-    "time"
-    "github.com/mgutz/ansi"
+import (
+	"bufio"
+	"fmt"
+	"github.com/mgutz/ansi"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
@@ -18,59 +18,58 @@ const (
 )
 
 func isCliClient(ua string) bool {
-    cliUA := [3]string{"curl", "Wget", "HTTPie"}
+	cliUA := [3]string{"curl", "Wget", "HTTPie"}
 
-    for _, elem := range cliUA {
-        if strings.HasPrefix(ua, elem) {
-            return true
-        }
-    }
+	for _, elem := range cliUA {
+		if strings.HasPrefix(ua, elem) {
+			return true
+		}
+	}
 
-    return false
+	return false
 }
 
 func render(w http.ResponseWriter, r *http.Request) {
 
-    green := ansi.ColorCode("green")
-    reset := ansi.ColorCode("reset")
+	green := ansi.ColorCode("green")
+	reset := ansi.ColorCode("reset")
 
-    f, err := os.Open("templates/adventure.txt")
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+	f, err := os.Open("templates/adventure.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
+	ua := r.Header["User-Agent"][0]
+	fmt.Println(ua)
 
-    ua := r.Header["User-Agent"][0]
-    fmt.Println(ua)
+	cli := isCliClient(ua)
+	if cli {
+		fmt.Fprintf(w, green)
+	} else {
+		fmt.Fprintf(w, HTMLSTART)
+	}
 
-    cli := isCliClient(ua)
-    if cli {
-        fmt.Fprintf(w, green)
-    } else {
-        fmt.Fprintf(w, HTMLSTART)    
-    }
+	scanner := bufio.NewScanner(f)
 
-    scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Fprintln(w, scanner.Text())
+		if fls, ok := w.(http.Flusher); ok {
+			fls.Flush()
+		} else {
+			fmt.Println("Damn, no flush")
+		}
+		time.Sleep(20 * time.Millisecond)
+	}
 
-    for scanner.Scan() {
-        fmt.Fprintln(w, scanner.Text())
-        if fls, ok := w.(http.Flusher); ok {
-            fls.Flush()
-        } else {
-            fmt.Println("Damn, no flush");
-        }
-        time.Sleep(20 * time.Millisecond)
-    }
-
-    if cli {
-        fmt.Fprintf(w, reset)
-    } else {
-        fmt.Fprintf(w, HTMLEND)    
-    }
+	if cli {
+		fmt.Fprintf(w, reset)
+	} else {
+		fmt.Fprintf(w, HTMLEND)
+	}
 }
 
 func main() {
-    http.HandleFunc("/adventure", render)
-    http.ListenAndServe(":9000", nil)
+	http.HandleFunc("/adventure", render)
+	http.ListenAndServe(":9000", nil)
 }
